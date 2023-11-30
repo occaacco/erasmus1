5afde22b53cbc60ac8068b4ff6051771dbaaf25f
HugyoZoltan <135591599+HugyoZoltan@users.noreply.github.com>
2023-11-30 10:25:47 +0100

    Add files via upload

---
+++ customaddons/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'Travel Management',
+    'vesion': '1.0',
+    'category': 'extra tools',
+    'depends': ['base', 'mail', 'uom'],
+    'description':"""
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/travel.xml',
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': True,
+}
