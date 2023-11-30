40445548996c2f71338ce2e85cb6be51b02be316
HugyoZoltan <135591599+HugyoZoltan@users.noreply.github.com>
2023-11-30 10:18:50 +0100

    Add files via upload

---
+++ customaddonstwo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'author': 'Your Name',
+    'category': 'Uncategorized',
+    'depends': ['base','mail','uom'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+
+}
---
+++ customaddonstwo/models/patient.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+class HospitalPatient(models.Model):
+    _name="hospital.patient"
+    description = "Patient Records"
+
+    name = fields.Char(string='Name',required=True)
+    age = fields.Integer(string="Age")
+    is_child = fields.Boolean(string="Is Child?")
+    notes =fields.Text(string="Notes")
+    gender =fields.Selection([("male","Male"),("female","Female"),("others","Others")], string="Gender")
